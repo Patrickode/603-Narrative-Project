99539b28b700d93b6a1f049b2ed3c2db2bc5c74c
Alfie Luo <yl5209@rit.edu>
2021-03-12 15:03:58 -0500

    Added Item_toggle.

--- Assets/Scripts/Item/Item_Toggle.cs
+++ Assets/Scripts/Item/Item_Toggle.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item_Toggle : MonoBehaviour
+/// <summary>
+/// This scrips allows player to toggle the active state of a game object, it is useful for things like light switch.
+/// 
+/// State: If you edit this value it will affect the inital state of the target object.
+/// Target: The object that you want the player to toggle.
+/// 
+/// </summary>
+
+public class Item_Toggle : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool state;
+    public GameObject target;
+
+    private void Awake()
     {
-        
+        if (target)
+            target.SetActive(state);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Interact()
     {
-        
+        state = !state;
+        if (target)
+            target.SetActive(state);
     }
 }
